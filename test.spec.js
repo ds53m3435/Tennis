538084e972858d371984c10537577aad59e8f5cf
Weerapat Chulaket <irksome_social@hotmail.co.th>
2016-09-26 19:58:57 +0700

    getScoreA

--- test.spec.js
+++ test.spec.js
@@ -5,46 +5,22 @@ function Tennis(){
 	    ScoreA = 0;
 	    ScoreB = 0;
   }
+	this.getScoreA = function() {
+
+	}
 	this.echo = function(){
-		if(ScoreA ===0 && ScoreB === 0){
+		if(ScoreA === 0 && ScoreB === 0){
 			return 'Love - Love';
+		}
+		else if(ScoreA === 15 && ScoreB === 0){
+			return 'Fifty - Love';
 		}
 	}
 }
-
-
 describe("Tennis", function() {
   it('should echo "Love - Love" when start game',function() {
     var tennis = new Tennis();
 		tennis.start();
     expect(tennis.echo()).toEqual('Love - Love');
   })
-	/*it('should echo "Fifteen - Love" when 15 - 0',function() {
-    var tennis = new Tennis();
-    expect(tennis.fifteenlove()).toEqual('Fifteen - Love');
-  })
-  it('should echo "Thirty - Love" when 30 - 0',function(){
-    var tennis = new Tennis();
-    expect(tennis.thirtylove()).toEqual('Thirty - Love');
-  })
-  it('should echo "Thirty - Love" when 40 - 0',function(){
-    var tennis = new Tennis();
-    expect(tennis.fourtylove()).toEqual('Fourty - Love');
-  })
-  it('should echo "Love - Fifteen" when 0 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.lovefifteen()).toEqual('Love - Fifteen');
-  })
-  it('should echo "Fifteen - Fifteen" when 15 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.fifteenfifteen()).toEqual('Fifteen - Fifteen');
-  })
-  it('should echo "Thirty - Fifteen" when 30 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.thirtyfifteen()).toEqual('Thirty - Fifteen');
-  })
-  it('should echo "Fourty - Fifteen" when 40 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.fourtyfifteen()).toEqual('Fourty - Fifteen');
-  })*/
 });
