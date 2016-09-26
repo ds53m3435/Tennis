bc0c36b9a969e43c808ab35a1eb9674275787f81
Kantapon Budda <vongolareborn@live.com>
2016-09-26 19:59:11 +0700

    add scoreB

--- test.spec.js
+++ test.spec.js
@@ -10,6 +10,9 @@ function Tennis(){
 			return 'Love - Love';
 		}
 	}
+  this.getScoreB = function(){
+    
+  }
 }
 
 
@@ -19,32 +22,5 @@ describe("Tennis", function() {
 		tennis.start();
     expect(tennis.echo()).toEqual('Love - Love');
   })
-	/*it('should echo "Fifteen - Love" when 15 - 0',function() {
-    var tennis = new Tennis();
-    expect(tennis.fifteenlove()).toEqual('Fifteen - Love');
-  })
-  it('should echo "Thirty - Love" when 30 - 0',function(){
-    var tennis = new Tennis();
-    expect(tennis.thirtylove()).toEqual('Thirty - Love');
-  })
-  it('should echo "Thirty - Love" when 40 - 0',function(){
-    var tennis = new Tennis();
-    expect(tennis.fourtylove()).toEqual('Fourty - Love');
-  })
-  it('should echo "Love - Fifteen" when 0 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.lovefifteen()).toEqual('Love - Fifteen');
-  })
-  it('should echo "Fifteen - Fifteen" when 15 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.fifteenfifteen()).toEqual('Fifteen - Fifteen');
-  })
-  it('should echo "Thirty - Fifteen" when 30 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.thirtyfifteen()).toEqual('Thirty - Fifteen');
-  })
-  it('should echo "Fourty - Fifteen" when 40 - 15',function(){
-    var tennis = new Tennis();
-    expect(tennis.fourtyfifteen()).toEqual('Fourty - Fifteen');
-  })*/
+
 });
